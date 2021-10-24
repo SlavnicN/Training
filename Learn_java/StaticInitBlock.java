4dbcd41fe89683dfd62fd3a33ee89c7402c9e098
Nikola Slavnic <slavnic.n@gmail.com>
2021-10-24 21:51:35 +0200

    add exo StaticInitBlock

---
+++ Learn_java/StaticInitBlock.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class StaticInitBlock{
+{
+    int flag =1;
+}
+    public static void main(String[] args) {
+		if(flag){
+            if(B<0 || H<0){
+			    System.out.print(area);
+            }else{
+			    int area=B*H;
+			    System.out.print(area);
+            }
+		}
+    }
+}
