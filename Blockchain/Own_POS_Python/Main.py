7f9d3e17ed610cfca96660626eeb5f3555058eb3
Nikola Slavnic <slavnic.n@gmail.com>
2021-11-10 17:05:28 +0100

    Implem of the AccountModel Class

--- Blockchain/Own_POS_Python/Main.py
+++ Blockchain/Own_POS_Python/Main.py
@@ -4,38 +4,15 @@ from TransactionPool import TransactionPool
 from Block import Block
 from Blockchain import Blockchain
 from BlockchainUtils import BlockchainUtils
+from AccountModel import AccountModel
 import pprint
 
 if __name__ == '__main__':
-    sender = 'sender'
-    receiver = 'receiver'
-    amount = 1
-    type = 'TRANSFER'
 
     wallet = Wallet()
-    fraudlentWallet = Wallet()
-    pool = TransactionPool()
+    accountModel = AccountModel()
 
-    transaction = wallet.createTransaction(receiver, amount, type)
+    accountModel.updateBalance(wallet.publicKeyString(),10)
 
-    if pool.transactionExists(transaction) == False:
-        pool.addTransaction(transaction)
-
-   # block = wallet.createBlock(pool.transactions, 'lastHash',1)
-    
-    blockchain = Blockchain()
-    lastHash = BlockchainUtils.hash(
-        blockchain.blocks[-1].payload()).hexdigest()
-    blockCount = blockchain.blocks[-1].blockCount + 1
-    block = wallet.createBlock(pool.transactions, lastHash, blockCount)
-
-    if not blockchain.lastBlockCountValid(block):
-        print('lastBlockHash is not valid')
-
-    if not blockchain.lastBlockHashValid(block):
-        print('BlockCount is not valid')
-
-    if blockchain.lastBlockCountValid(block) and blockchain.lastBlockHashValid(block):
-        blockchain.addBlock(block)
-
-    pprint.pprint(blockchain.toJson())
+    accountModel.updateBalance(wallet.publicKeyString(),-5)
+    print(accountModel.balances)
