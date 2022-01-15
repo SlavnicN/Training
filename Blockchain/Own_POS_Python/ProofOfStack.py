606d571625eb16c732ecfdcb624014ecb6b10cd0
Slavnic <slavnic.n@gmail.com>
2022-01-15 19:07:54 +0100

    Add forger fonction

--- Blockchain/Own_POS_Python/ProofOfStack.py
+++ Blockchain/Own_POS_Python/ProofOfStack.py
@@ -1,4 +1,5 @@
-
+from Lot import Lot
+from BlockchainUtils import BlockchainUtils
 
 class ProofOfStack():
 
@@ -15,3 +16,31 @@ class ProofOfStack():
         if  publicKeyString in self.stakers.keys():
             return self.stakers[publicKeyString]
         return None
+
+    def validatorLot(self, seed):
+        lots = []
+        for validator in self.stakers.keys():
+            for stack in range(self.get(validator)):
+                lots.append(Lot(validator, stack+1, seed))
+        return lots
+
+    def winnerLot(self, lots, seed):
+        winnerLot = None
+        leastOffset = None
+        referenceHashIntValue = int(BlockchainUtils.hash(seed).hexdigest(), 16)
+        print('This is the reference hash value: ', referenceHashIntValue)
+        for lot in lots:
+            lotIntValue = int(lot.lotHash(), 16)
+            offset = abs(referenceHashIntValue - lotIntValue)
+            if leastOffset is None or offset < leastOffset:
+                leastOffset = offset
+                winnerLot = lot
+        print('This is the lot hash value: ', lotIntValue)
+        print('This is the least offset: ', leastOffset)
+
+        return winnerLot
+
+    def forger(self, lastBlock):
+        lots = self.validatorLot(lastBlock)
+        winnerLot = self.winnerLot(lots, lastBlock)
+        return winnerLot.publicKey
--- Blockchain/Own_POS_Python/Test.py
+++ Blockchain/Own_POS_Python/Test.py
@@ -1,13 +1,27 @@
 from ProofOfStack import ProofOfStack
 from Lot import Lot
+import string
+import random
 
+def getRandomString(length):
+    letters = string.ascii_lowercase
+    randomString = ''.join(random.choice(letters) for i in range(length))
+    return randomString 
+    
 if __name__ == '__main__':
-    '''pos = ProofOfStack()
+    pos = ProofOfStack()
     pos.update('bob', 10)
-    pos.update('alice', 1000)
-    print(pos.get('bob'))
-    print(pos.get('alice'))
-    print(pos.get('jack'))'''
+    pos.update('alice', 100)
 
-    lot = Lot('bob', 1 ,'lastHash')
-    print(lot.lotHash())
+    bobWins = 0
+    aliceWins = 0
+
+    for i in range(10):
+        forger = pos.forger(getRandomString(i))
+        if forger == 'bob':
+            bobWins += 1
+        else:
+            aliceWins += 1
+    
+    print('bob wins: ' + str(bobWins) + 'times')
+    print('alcie wins: ' + str(aliceWins) + 'times')
